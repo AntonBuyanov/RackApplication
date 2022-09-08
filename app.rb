7490919b2c3160c99a86f73702d9e81f36b58a7b
AntonBuyanov <102428399+AntonBuyanov@users.noreply.github.com>
2022-09-08 17:33:04 +0600

    feat: Rack application

--- app.rb
+++ app.rb
@@ -1,26 +1,24 @@
-class App
+require 'rack'
+require_relative 'time_format'
 
+class App
   def call(env)
-    perform_request
-    [status, headers, body]
+    @request = Rack::Request.new(env)
+
+    return response(404, 'Not Found 404') unless @request.path == '/time'
+
+    format = TimeFormat.new(@request.params['format'])
+
+    if format.valid?
+      response(200, format.time_request)
+    else
+      response(400, "Unknown time format #{format.unknown_params}")
+    end
   end
 
   private
 
-  def perform_request
-    sleep rand(2..3)
+  def response(status, body)
+    [status, { 'Content-Type' => 'text/plain' }, [body]]
   end
-
-  def status
-    200
-  end
-
-  def headers
-    { 'Content-Type' => 'text/plain' }
-  end
-
-  def body
-    ["Welcome aboard!\n"]
-  end
-
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,3 @@
-require_relative 'middleware/logger'
 require_relative 'app'
 
-use AppLogger, logdev: File.expand_path('log/app.log', __dir__)
 run App.new
